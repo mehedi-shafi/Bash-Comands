5aa5a707be7d5e9eecee46ce543767171ccbd855
Mehedi Shafi <mehedishafi@hotmail.com>
2017-07-27 10:18:01 +0600

    Update run.sh

--- C CPP run/run.sh
+++ C CPP run/run.sh
@@ -2,18 +2,28 @@
 
 #run your C or C++ code directly from the terminal
 
-if [ $# -ne 1 ]
+if [ $# -ne 2 ]
 then
-    echo "Please Provide the file name.. It's required"
+    echo "Please Provide all the parameters.. It's required"
     clear
-    exit
+    return
 fi
 
 filename=$1
+filetype=$2
 
 #compile
-gcc $filename -o $filename+out
-
+if [ $filetype = "cpp" ]
+then 
+    c++ $filename -o $filename+out
+elif [ $filetype = "c" ]
+then 
+    gcc $filename -o $filename+out
+else
+    echo "File type is not supported."
+    clear
+    return
+fi
 
 #run
 ./$filename+out
